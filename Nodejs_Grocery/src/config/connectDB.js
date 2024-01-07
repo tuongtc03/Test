3e56d1fc3f170d58a1ca3716226496ffe7b4bca0
tuongtc03 <tuantuong326@gmail.com>
2024-01-07 14:41:14 +0700

    setup dotenv

--- Nodejs_Grocery/src/config/connectDB.js
+++ Nodejs_Grocery/src/config/connectDB.js
@@ -1,11 +1,17 @@
 const { Sequelize } = require("sequelize");
+require("dotenv").config();
 
 // Option 3: Passing parameters separately (other dialects)
-const sequelize = new Sequelize("db_taphoa", "root", null, {
-  host: "localhost",
-  dialect: "mysql",
-  logging: false,
-});
+const sequelize = new Sequelize(
+  process.env.DB_DATABASE_NAME,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST2,
+    dialect: process.env.DB_DIALECT,
+    logging: false,
+  }
+);
 
 let connectDB = async () => {
   try {
